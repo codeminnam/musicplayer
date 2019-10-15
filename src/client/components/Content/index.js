3f58365ef75bb954818f094eb9292b579b9752ed
cineminnam <cine.minnam@gmail.com>
2019-10-16 01:29:44 +0900

    added_youtube_player
    added youtube player

--- src/client/components/Content/index.js
+++ src/client/components/Content/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./styles.css";
 import { Chip } from "../Chip";
+import YouTube from "react-youtube";
 
 export function Content(props) {
   return (
     <div className="content">
+      <YouTube 
+        videoId="e8DfkZ0HDXE"
+      />
       {props.selectedItems.map((item, index) => {
         return (
-          <Chip
-            key={index}
-            item={item}
-            className="playlist-item"
-            onDelete={props.onDeleteSelectedItems}
-          />
+            <Chip
+              key={index}
+              item={item}
+              className="playlist-item"
+              onDelete={props.onDeleteSelectedItems}
+            />
         );
       })}
     </div>
